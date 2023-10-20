ec7644a98501d77a45a3cc545d42b5033aad6473
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-10-20 20:38:29 +0200

    ajout css

--- FrontEnd/components/CustomSelect.js
+++ FrontEnd/components/CustomSelect.js
@@ -1,64 +1,64 @@
-export const CustomSelectUI = (filter_categories) => {
+export const CustomSelectUI = (categories, checkFields) => {
     const add_work_select_category = document.createElement('select');
     add_work_select_category.name = 'category';
     add_work_select_category.id = 'category';
     add_work_select_category.classList.add('add-work__custom-select');
 
-    filter_categories.forEach((category) => {
+    categories.forEach((category) => {
         if (category !== 'Tous') {
             const add_work_select_category_options = document.createElement('option');
-            add_work_select_category_options.value = category;
-            add_work_select_category_options.innerText = category;
+            add_work_select_category_options.value = category.id;
+            add_work_select_category_options.innerText = category.name;
             add_work_select_category_options.classList.add('add-work__select-options');
             add_work_select_category.appendChild(add_work_select_category_options);
         }
     });
 
-    const add_work_custom_select = document.createElement('div');
-    add_work_custom_select.classList.add('add-work__input', 'add-work__input--select');
+    const custom_select = document.createElement('div');
+    custom_select.classList.add('add-work__input', 'custom-select', 'custom-select__wrapper');
     let isOpen = false;
 
-    const add_work_custom_select_value = document.createElement('span');
-    add_work_custom_select_value.classList.add('add-work__custom-select-value');
+    const custom_select_value = document.createElement('p');
+    custom_select_value.classList.add('custom-select__value');
 
-    const add_work_custom_select_items = document.createElement('ul');
-    add_work_custom_select_items.classList.add('add-work__custom-select-items');
+    const custom_select_items = document.createElement('ul');
+    custom_select_items.classList.add('custom-select', 'custom-select__list');
 
-    filter_categories.forEach((category) => {
+    categories.forEach((category) => {
         if (category !== 'Tous') {
-            const add_work_custom_select_item = document.createElement('li');
-            add_work_custom_select_item.classList.add('add-work__custom-select-item');
-            add_work_custom_select_item.innerText = category;
-            add_work_custom_select_items.appendChild(add_work_custom_select_item);
+            const custom_select_item = document.createElement('li');
+            custom_select_item.classList.add('custom-select__item');
+            custom_select_item.innerText = category.name;
+            custom_select_items.appendChild(custom_select_item);
 
-            add_work_custom_select_item.addEventListener('click', () => {
-                console.log('Catégorie sélectionnée :', category);
-                add_work_select_category.value = category;
-                add_work_custom_select_value.innerText = category;
-                console.log('Catégorie sélectionnée du formulaire:', add_work_select_category.value);
+            custom_select_item.addEventListener('click', () => {
+                add_work_select_category.value = category.id;
+                custom_select_value.innerText = category.name;
+                checkFields();
                 if (isOpen) {
-                    add_work_custom_select.classList.remove('active');
-                    add_work_custom_select_items.classList.remove('display');
+                    custom_select.classList.remove('opened');
+                    custom_select_items.classList.remove('display');
                     isOpen = !isOpen;
                 }
             });
         }
     });
+    
 
-    add_work_custom_select.addEventListener('click', () => {
+    custom_select.addEventListener('click', () => {
         isOpen = !isOpen;
         if (!isOpen) {
-            add_work_custom_select.classList.add('active');
-            add_work_custom_select_items.classList.add('display');
+            custom_select.classList.add('opened');
+            custom_select_items.classList.add('display');
         } else {
-            add_work_custom_select.classList.remove('active');
-            add_work_custom_select_items.classList.remove('display');
+            custom_select.classList.remove('opened');
+            custom_select_items.classList.remove('display');
         }
     });
 
-    add_work_custom_select.appendChild(add_work_custom_select_value);
-    add_work_custom_select.appendChild(add_work_custom_select_items);
-    add_work_custom_select.appendChild(add_work_select_category);
+    custom_select.appendChild(custom_select_value);
+    custom_select.appendChild(custom_select_items);
+    custom_select.appendChild(add_work_select_category);
 
-    return add_work_custom_select;
+    return {custom_select, add_work_select_category};
 }
