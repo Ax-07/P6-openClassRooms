049c0ea05274a732549b662803db4fcd373e1d01
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-10-20 12:07:59 +0200

    ajout css header nav footer et correction fautes orthographe

--- FrontEnd/components/Navbar.js
+++ FrontEnd/components/Navbar.js
@@ -1,17 +1,33 @@
+import { Logout } from '../api/Log.js'
+
 export const Navbar = () => {
     const user = localStorage.getItem('token');
+
     if (user) {
-        
+        const log_link = document.querySelector('.navbar__item--log');
+        log_link.innerText = 'Logout';
+        log_link.href = '#';
+        log_link.addEventListener('click', (e) => {
+            e.preventDefault();
+            Logout();
+        });
     }
 
-    document.querySelectorAll('.link').forEach((link) => {
-        link.addEventListener('click', () => {
-            link.setAttribute('style', 'font-weight: 900;');
-            document.querySelectorAll('.link').forEach((otherLink) => {
-                if (otherLink !== link) {
-                    otherLink.setAttribute('style', 'font-weight: 400;');
-                }
+    const nav_list_items_link = document.querySelectorAll('.navbar__link');
+    console.log('nav_list_items_link:', nav_list_items_link);
+    nav_list_items_link.forEach((link) => {
+        if (link.href === window.location.href) {
+            link.classList.add('navbar__link--active');
+            console.log('link.href origin:', link.href);
+        }
+        link.addEventListener('click', (e) => {
+            nav_list_items_link.forEach((item) => {
+                item.classList.remove('navbar__link--active');
             });
+
+            console.log('link.href:', link.href);
+            console.log('window.location.href:', window.location.href);
+            link.classList.add('navbar__link--active');
         });
     });
 };
