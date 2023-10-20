b7140a52bd4a800f09304a9d6108d518bcb4e151
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-10-20 20:37:53 +0200

    modif prop modal

--- FrontEnd/components/modal/Modal.js
+++ FrontEnd/components/modal/Modal.js
@@ -4,7 +4,7 @@ import { ModalEvent } from './ModalEvent.js';
 import { Gallery_modal } from '../../containers/Gallery.js';
 import { AddWorkFormUI } from './AddWorkForm.js';
 
-export const Modal = (works, filter_categories) => {
+export const Modal = (works, categories) => {
     const body = document.querySelector('body');
 
     const {
@@ -21,7 +21,7 @@ export const Modal = (works, filter_categories) => {
     ModalEvent(modal, modal_background,modal_back, modal_close, modal_gallery, modal_add_picture, modal_title, modal_btn);
 
     Gallery_modal(works, modal_gallery);
-    const add_work = AddWorkFormUI(filter_categories);
+    const add_work = AddWorkFormUI(categories);
     modal_add_picture.appendChild(add_work);
 
     body.appendChild(modal_background);
