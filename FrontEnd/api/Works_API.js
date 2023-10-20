7b9eafa033421fa98eeae3adbf6faea459409961
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-10-20 12:12:34 +0200

    ajout fonction refresh data

--- FrontEnd/api/Works_API.js
+++ FrontEnd/api/Works_API.js
@@ -1,3 +1,6 @@
+import { createPicture } from "../components/Picture.js";
+import { CloseModal } from "../components/modal/ModalEvent.js";
+
 export const All_data = async () => {
     const categories_url = "http://localhost:5678/api/categories";
     const works_url = "http://localhost:5678/api/works";
@@ -18,30 +21,66 @@ export const All_data = async () => {
 
     return { categoriesData, worksData };
 };
-export const Create_data = async (data) => {
+
+export const Refresh_Data = async () => {
+    const refresh_data = await All_data();
+    updateGallery(refresh_data);
+
+    function updateGallery(refresh_data) {
+        const gallery = document.querySelector(".gallery");
+        gallery.innerHTML = "";
+        const modal_gallery = document.querySelector(".modal__gallery");
+        modal_gallery.innerHTML = "";
+        refresh_data.worksData.forEach((work) => {
+            const figure = createPicture(work, false);
+            gallery.appendChild(figure);
+            const figure_modal = createPicture(work, true);
+            modal_gallery.appendChild(figure_modal);
+        });
+    }
+}
+
+export const Create_data = async (form_data) => {
     const works_url = "http://localhost:5678/api/works";
     try {
         const response = await fetch(works_url, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token"),
             },
-            body: JSON.stringify(data),
+            body: form_data,
         });
-        const data = await response.json();
-        console.log("data :", data);
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log("data :", data);
+            Refresh_Data();
+            CloseModal();
+        } else {
+            console.error("Échec de la création");
+        }
     } catch (error) {
         console.error("Une erreur s'est produite lors de la création des données :", error);
     }
 }
+
 export const Delete_data = async (id) => {
     const works_url = "http://localhost:5678/api/works";
+    console.log("works_url :", works_url);
+    console.log("id :", id);
     try {
         const response = await fetch(works_url + "/" + id, {
             method: "DELETE",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("token"),
+            },
         });
-        const data = await response.json();
-        console.log("data :", data);
+        if (response.ok) {
+            Refresh_Data();
+        }
+        else {
+            console.error("Échec de la suppression");
+        }
     } catch (error) {
         console.error("Une erreur s'est produite lors de la suppression des données :", error);
     }
