5bf660848bf20d620613265379d6a52aa040bcaf
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-06 11:10:40 +0100

    transfert d'element creer via js vers html

--- FrontEnd/components/modal/Edit_modal.js
+++ FrontEnd/components/modal/Edit_modal.js
@@ -1,20 +1,16 @@
+const edit_banner = document.querySelector('.edit__banner');
+const edit_btn = document.querySelector('.edit__btn');
+const modal = document.querySelector('.modal');
+const modal_background = document.querySelector('.modal__background');
+
 export const Edit_modal = () => {
     const user = localStorage.getItem('token');
     if (!user) {
         return;
     }
-    const edit_banner = document.querySelector('.edit__banner');
     edit_banner.setAttribute('style', 'display: flex;');
 
-    const edit_btn = document.createElement('span');
-    edit_btn.classList.add('edit__btn');
-    edit_btn.innerText = 'Mode édition';
-
-
-
-    edit_btn.addEventListener('click', (e) => {
-        const modal = document.querySelector('.modal');
-        const modal_background = document.querySelector('.modal__background');
+    edit_btn.addEventListener('click', () => {
         modal.setAttribute('style', 'display: flex;');
         modal_background.setAttribute('style', 'display: flex;');
     });
