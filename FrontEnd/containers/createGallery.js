ca9959e3e74a96b6a9be730e4f22c5323509d92d
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-07 11:51:07 +0100

    renommage fichier

--- FrontEnd/containers/createGallery.js
+++ FrontEnd/containers/createGallery.js
@@ -1,6 +1,8 @@
 import { createCard } from "../components/createWork.js";
+import { All_data } from "../api/Works_API.js";
 
 const gallery = document.querySelector('.gallery');
+
 
 const filterWorks = (works, selectedFilter) => {
     return selectedFilter === "Tous" ? works : works.filter(work => work.category.name === selectedFilter);
@@ -13,7 +15,10 @@ const createGalleryContent = (gallery, filteredWorks, isInModal) => {
     });
 }
 
-export const createGallery = (works, selectedFilter) => {
+export const createGallery = async (selectedFilter) => {
+    const datas = await All_data();
+    const works = datas.worksData;
+    // const works = store()?.works; console.log("works :", works);
     const updateGallery = () => {
         const filteredWorks = filterWorks(works, selectedFilter);
         createGalleryContent(gallery, filteredWorks, false);
@@ -24,6 +29,7 @@ export const createGallery = (works, selectedFilter) => {
 };
 
 export const createGalleryModal = (works, modalGallery) => {
+    console.log("works :", works);
     works.forEach(work => {
         const modalCard = createCard(work, true);
         modalGallery.appendChild(modalCard);
