6ad4a2f507f4c0850e71f5b013fdafaeb3731d50
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-07 11:58:59 +0100

    refactoring

--- FrontEnd/components/modal/Edit_modal.js
+++ FrontEnd/components/modal/Edit_modal.js
@@ -3,17 +3,21 @@ const edit_btn = document.querySelector('.edit__btn');
 const modal = document.querySelector('.modal');
 const modal_background = document.querySelector('.modal__background');
 
+const displayEditBanner = () => {
+    edit_banner.style.display = 'flex';
+}
+const openModal = () => {
+    modal.style.display = 'flex';
+    modal_background.style.display = 'flex';
+}
+
 export const Edit_modal = () => {
     const user = localStorage.getItem('token');
     if (!user) {
         return;
     }
-    edit_banner.setAttribute('style', 'display: flex;');
-
-    edit_btn.addEventListener('click', () => {
-        modal.setAttribute('style', 'display: flex;');
-        modal_background.setAttribute('style', 'display: flex;');
-    });
+    displayEditBanner();
+    edit_btn.addEventListener('click', () => openModal());
 
     edit_banner.appendChild(edit_btn);
 }
