3063b987717e039358b37c358a294ab412d396c1
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-08 11:11:38 +0100

    refactoring et modif gestion erreur

--- FrontEnd/api/Log.js
+++ FrontEnd/api/Log.js
@@ -1,52 +1,72 @@
+const login_url = "http://localhost:5678/api/users/login";
+
+const email_error = document.querySelector("#email_error");
+const password_error = document.querySelector("#password_error");
+const login_input_email = document.querySelector('#email');
+const login_input_password = document.querySelector('#password');
+
+const displayLoginError = {
+    email : () => {
+        login_input_email.classList.add('login__input--error');
+        email_error.classList.add('login__error--display');
+        email_error.textContent = "❌ Email incorrect";
+    },
+    password : () => {
+        login_input_password.classList.add('login__input--error')
+        password_error.classList.add('login__error--display');
+    },
+}
+function setUserToken(token) {
+    localStorage.setItem("token", token);
+}
+function removeUserToken() {
+    localStorage.removeItem("token");
+}
+function redirectHome() {
+    window.location.href = "http://localhost:5500";
+}
+
 // Fonction pour envoyer les données de connexion au serveur
-export const Login = () => {
-    const login_url = "http://localhost:5678/api/users/login";
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
+export const login = async (formData) => {
+    try {
+        const response = await fetch(login_url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        });
 
-    const formData = {
-        email: email, // sophie.bluel@test.tld
-        password: password // S0phie
-    };
-
-    fetch(login_url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-    })
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            console.error("Échec de la connexion");
+        if (!response.ok) {
+            if (response.status === 404) {
+                displayLoginError.email();
+                return;
+            } else if (response.status === 401) {
+                displayLoginError.password();
+                return;
+            } else {
+                console.error("Erreur inattendue : ", error);
+            }
         }
-    })
-    .then((data) => {
-        const userId = data.userId;
+
+        const data = await response.json();
         const token = data.token;
-        console.log("userId: ",userId);
-        console.log("token: ",token);
-        console.log("Connexion réussie !");
-        localStorage.setItem("userId", userId);
-        localStorage.setItem("token", token);
-        window.location.href = "http://localhost:5500";
-    })
-    .catch((error) => {
-        console.error("Erreur inattendue : " + error);
-    });
+
+        if ( token ) {
+            setUserToken(token);
+            redirectHome();
+        }
+    } catch (error) {
+        console.error("Erreur inattendue : ", error);
+    }
 };
 
-export const Logout = () => {
+export const logout = () => {
     try {
-        const token = localStorage.getItem("token");
-        console.log("token: ",token);
+        removeUserToken();
+        redirectHome();
         console.log("Déconnexion réussie !");
-        localStorage.removeItem("token");
-        window.location.href = "http://localhost:5500";
+    } catch (error) {
+        console.error("Erreur inattendue : ", error);
     }
-    catch (error) {
-        console.error("Erreur inattendue : " + error);
-    }
-}
+};
