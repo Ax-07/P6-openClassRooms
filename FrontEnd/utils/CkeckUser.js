57a204cc057eda4b4efdd37829a65535a1a829eb
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-08 14:22:38 +0100

    delete fichier

--- FrontEnd/utils/CkeckUser.js
+++
@@ -1,8 +0,0 @@
-const CheckUser = () => {
-    const user = localStorage.getItem('user');
-    if (user) {
-        console.log("user: ",JSON.parse(user));
-        return JSON.parse(user);
-    }
-    return null;
-}
