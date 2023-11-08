5b5a65b7d6f765164f9f41184f64a39179cf8d26
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-08 14:24:40 +0100

    delete fichier

--- FrontEnd/pages/Login.js
+++
@@ -1,8 +0,0 @@
-import { Navbar } from '../components/Navbar.js';
-import { LoginForm } from '../components/logs/LoginForm.js';
-
-const Login = () => {
-    LoginForm();
-    Navbar();
-}
-window.onload = Login;
