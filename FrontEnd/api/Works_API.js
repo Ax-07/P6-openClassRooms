c11b30738ead3ef34505703523f5c98db2dd219e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-11 12:01:54 +0100

    reorganisation et suppression code inutile

--- FrontEnd/api/Works_API.js
+++ FrontEnd/api/Works_API.js
@@ -1,60 +1,40 @@
-import { createCard } from "../components/createWork.js";
 import { modalEvent } from "../components/modal/Modal.js";
+import { createGal } from "../containers/createGallery.js";
+import { addNewWorkToStore, deleteWorkFromStore, user } from "./store.js";
 
-export const All_data = async () => {
-    const categories_url = "http://localhost:5678/api/categories";
-    const works_url = "http://localhost:5678/api/works";
+const baseUrl = "http://localhost:5678";
+const categoriesUrl = `${baseUrl}/api/categories`;
+const worksUrl = `${baseUrl}/api/works`;
 
-    let categoriesData, worksData;
-
+export const getAllData = async () => {
     try {
-        const responseCategories = await fetch(categories_url);
-        categoriesData = await responseCategories.json();
+        const responseCategories = await fetch(categoriesUrl);
+        const categoriesData = await responseCategories.json();
+        sessionStorage.setItem("categories", JSON.stringify(categoriesData));
 
-        const responseWorks = await fetch(works_url);
-        worksData = await responseWorks.json();
+        const responseWorks = await fetch(worksUrl);
+        const worksData = await responseWorks.json();
+        sessionStorage.setItem("works", JSON.stringify(worksData));
 
     } catch (error) {
         console.error("Une erreur s'est produite lors de la récupération des données :", error);
     }
-
-    return { categoriesData, worksData };
 };
 
-export const Refresh_Data = async () => {
-    const refresh_data = await All_data();
-    updateGallery(refresh_data);
-
-    function updateGallery(refresh_data) {
-        const gallery = document.querySelector(".gallery");
-        gallery.innerHTML = "";
-        const modal_gallery = document.querySelector(".modal__gallery");
-        modal_gallery.innerHTML = "";
-        refresh_data.worksData.forEach((work) => {
-            const figure = createCard(work, false);
-            gallery.appendChild(figure);
-            const figure_modal = createCard(work, true);
-            modal_gallery.appendChild(figure_modal);
-        });
-    }
-}
-
-export const Create_data = async (form_data) => {
-    const works_url = "http://localhost:5678/api/works";
-    const token =localStorage.getItem("token");
+export const createData = async (form_data) => {
     try {
-        const response = await fetch(works_url, {
+        const response = await fetch(worksUrl, {
             method: "POST",
             headers: {
-                "Authorization": "Bearer " + token,
+                "Authorization": "Bearer " + user.token,
             },
             body: form_data,
         });
 
         if (response.ok) {
             const data = await response.json();
-            console.log("data :", data);
-            Refresh_Data();
+            addNewWorkToStore(data);
+            createGal();
             modalEvent.closeModal();
         } else {
             console.error("Échec de la création");
@@ -64,19 +44,17 @@ export const Create_data = async (form_data) => {
     }
 }
 
-export const Delete_data = async (id) => {
-    const works_url = "http://localhost:5678/api/works";
-    console.log("works_url :", works_url);
-    console.log("id :", id);
+export const deleteData = async (id) => {
     try {
-        const response = await fetch(works_url + "/" + id, {
+        const response = await fetch(worksUrl + "/" + id, {
             method: "DELETE",
             headers: {
-                "Authorization": "Bearer " + localStorage.getItem("token"),
+                "Authorization": "Bearer " + user.token,
             },
         });
         if (response.ok) {
-            Refresh_Data();
+            deleteWorkFromStore(id);
+            createGal();
         }
         else {
             console.error("Échec de la suppression");
