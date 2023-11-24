11453d2a51d1e408b331c7b4d692d30e895e3c9b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-24 14:58:05 +0100

    separation responsabilité et integration bus event

--- FrontEnd/components/LoginForm.js
+++ FrontEnd/components/LoginForm.js
@@ -1,100 +1,52 @@
+import { displayLoginError, loginError } from "../errors/loginErrors.js";
 import { login } from "../services/api/logsApi.js";
+import { userBus } from "../services/eventBus.js";
+import { loginSubscription } from "../services/events/loginSubscription.js";
 
 const login_input_email = document.querySelector('#email');
 const login_input_password = document.querySelector('#password');
 const login_form = document.querySelector('#login_form');
-const email_error = document.querySelector('#email_error');
-const password_error = document.querySelector("#password_error");
 
-const displayLoginError = {
-    email: () => {
-        login_input_email.classList.add('login__input--error');
-        email_error.classList.add('login__error--display');
-        email_error.textContent = "❌ Email incorrect";
-    },
-    password: () => {
-        login_input_password.classList.add('login__input--error');
-        password_error.classList.add('login__error--display');
-    },
-}
-// Fonction pour créer le formulaire de connexion
-export const loginForm = () => {
-    login_input_email.addEventListener('input', function () {
-        const emailValue = login_input_email.value;
-        const isValid = checkFormatEmail(emailValue);
-        if (!isValid) {
-            setErrorStyle.displayError();
-        }
-        else {
-            setErrorStyle.hideError();
-        }
-    });
-    login_form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        if (!checkFormatEmail(login_input_email.value)) {
-            return;
-        }
-        handleSubmit(e);
-        console.log("Connexion");
-    });
+loginSubscription();
+
+const loginForm = () => {
+    login_input_email.addEventListener('input', checkEmailFormat);
+    login_form.addEventListener('submit', onSubmit);
 };
 
-function checkFormatEmail(email) {
+function checkEmailFormat({target: {value: email}}) {
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     let isValid = emailRegex.test(email);
 
-    return isValid;
+    if (!isValid) {
+        loginError.invalidEmail();
+    }
+    else {
+        loginError.hideError();
+    }
 };
 
-function handleSubmit(e) {
+async function onSubmit(e) {
     e.preventDefault();
     const emailValue = login_input_email.value; // sophie.bluel@test.tld
     const passwordValue = login_input_password.value; // S0phie
+
+    // Vérifie à nouveau si tous les champs sont remplis
+    if (!emailValue || !passwordValue) {
+        return;
+    }
+
     const data = {
         email: emailValue,
         password: passwordValue
     };
-    // Vérifiez à nouveau si tous les champs sont remplis
-    if (emailValue && passwordValue) {
-        login(data)
-            .then(async (response) => {
-                if (!response.ok) {
-                    if (response.status === 404) {
-                        displayLoginError.email();
-                        return;
-                    } else if (response.status === 401) {
-                        displayLoginError.password();
-                        return;
-                    } else {
-                        console.error("Erreur inattendue : ", error);
-                    }
-                }
 
-                const data = await response.json();
-                const token = data.token;
-
-                if (token) {
-                    setUserToken(token);
-                    redirectHome();
-                }
-            });
+    try {
+        const response = await login(data);
+        userBus.emit("login", response);
+    } catch (error) {
+        console.error("Erreur lors de la connexion :", error);
     }
-};
+}
 
-const setErrorStyle = {
-    displayError: () => {
-        login_input_email.classList.add('login__input--error');
-        email_error.classList.add('login__error--display');
-    },
-    hideError: () => {
-        login_input_email.classList.remove('login__input--error');
-        email_error.classList.remove('login__error--display');
-    }
-};
-function setUserToken(token) {
-    localStorage.setItem("token", token);
-}
-function redirectHome() {
-    window.location.href = "../index.html";
-}
 loginForm();
