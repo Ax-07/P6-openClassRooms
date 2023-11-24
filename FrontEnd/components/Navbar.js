036517c6dc02e53df5ac689362db5d92f2431171
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-24 14:56:06 +0100

    refactoring

--- FrontEnd/components/Navbar.js
+++ FrontEnd/components/Navbar.js
@@ -1,18 +1,14 @@
 import { logout } from '../services/api/logsApi.js';
 
-const user = localStorage.getItem('token');
-export const initLogoutLink = () => {
-    if (user) {
-        const log_link = document.querySelector('.navbar__item--log');
-        log_link.innerText = 'Logout';
-        log_link.href = '#';
-        log_link.addEventListener('click', (e) => {
-            e.preventDefault();
-            logout();
-        });
-    } 
+export const setLogoutLink = () => {
+    const log_link = document.querySelector('.navbar__item--log');
+    log_link.innerText = 'Logout';
+    log_link.href = '#';
+    log_link.addEventListener('click', (e) => {
+        e.preventDefault();
+        logout();
+    });
 }
-initLogoutLink();
 
 export const setActiveLink = () => {
     const nav_list_items_link = document.querySelectorAll('.navbar__link');
@@ -29,34 +25,4 @@ export const setActiveLink = () => {
             link.classList.add('navbar__link--active');
         });
     });
-}
-setActiveLink();
-
-export const Navbar = (user) => {
-
-    if (user) {
-        const log_link = document.querySelector('.navbar__item--log');
-        log_link.innerText = 'Logout';
-        log_link.href = '#';
-        log_link.addEventListener('click', (e) => {
-            e.preventDefault();
-            logout();
-        });
-    }
-
-    const nav_list_items_link = document.querySelectorAll('.navbar__link');
-
-    nav_list_items_link.forEach((link) => {
-        if (link.href === window.location.href) {
-            link.classList.add('navbar__link--active');
-        }
-
-        link.addEventListener('click', () => {
-            nav_list_items_link.forEach((item) => {
-                item.classList.remove('navbar__link--active');
-            });
-            link.classList.add('navbar__link--active');
-        });
-    });
 };
-Navbar();
