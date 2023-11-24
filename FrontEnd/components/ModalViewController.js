d6122eb7f17b829af57d8c989f83035c1f961662
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-24 14:38:14 +0100

    changement nom de constante

--- FrontEnd/components/ModalViewController.js
+++ FrontEnd/components/ModalViewController.js
@@ -6,7 +6,7 @@ const modal = document.querySelector('.modal');
 const modal_btn_back = document.querySelector('.modal__btn-back');
 const modal_btn_close = document.querySelector('.modal__btn-close');
 const modal_gallery = document.querySelector('.modal__gallery');
-const modal_add_picture = document.querySelector('.modal__add-work');
+const modal_add_work = document.querySelector('.modal__add-work');
 const modal_title = document.querySelector('.modal__title');
 const modal_btn_addWork = document.querySelector('.modal__btn');
 
@@ -20,21 +20,21 @@ export const modalEvent = {
         modal_background.setAttribute('style', 'display: none;');
         modal_btn_back.setAttribute('style', 'display: none;');
         modal_gallery.setAttribute('style', 'display: flex;');
-        modal_add_picture.setAttribute('style', 'display: none;');
+        modal_add_work.setAttribute('style', 'display: none;');
         modal_title.innerText = 'Galerie photo';
         modal_btn_addWork.setAttribute('style', 'display: block;');
     },
     displayAddWorkForm() {
         modal_btn_back.setAttribute('style', 'display: flex;');
         modal_gallery.setAttribute('style', 'display: none;');
-        modal_add_picture.setAttribute('style', 'display: flex;');
+        modal_add_work.setAttribute('style', 'display: flex;');
         modal_title.innerText = 'Ajout photo';
         modal_btn_addWork.setAttribute('style', 'display: none;');
     },
     backGallery() {
         modal_btn_back.setAttribute('style', 'display: none;');
         modal_gallery.setAttribute('style', 'display: flex;');
-        modal_add_picture.setAttribute('style', 'display: none;');
+        modal_add_work.setAttribute('style', 'display: none;');
         modal_title.innerText = 'Galerie photo';
         modal_btn_addWork.setAttribute('style', 'display: block;');
     }
@@ -56,7 +56,7 @@ export const Modal = () => {
 
     const add_work = addWorkForm();
 
-    modal_add_picture.appendChild(add_work);
+    modal_add_work.appendChild(add_work);
 
     body.appendChild(modal_background);
     body.appendChild(modal);
