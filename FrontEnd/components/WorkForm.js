4ceeecb4c44a9f24975799a6b1f5920c2486065b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-11-30 16:11:48 +0100

    modif en objet class et ajouts bus d'evenement

---
+++ FrontEnd/components/WorkForm.js
@@ -0,0 +1,52 @@
+import { workFormBus } from "../services/eventBus.js";
+
+const title = document.querySelector('.add-work__input--title');
+
+
+class WorkForm {
+    constructor() {
+        this._addWorkForm = document.querySelector("#add_work");
+        this._picture = '';
+        this._title = '';
+        this._category = '';
+        this.form_data = new FormData();
+
+        workFormBus.subscribe('workForm:setFormData', (formData) => {
+            console.log('workForm:set form data', formData);
+            this._picture = formData.picture; console.log('picture:', this._picture);
+            this._title = formData.title; console.log('title:', this._title);
+            this._category = formData.category; console.log('category:', this._category);
+        });
+    }
+
+    resetForm() {
+        title.value = '';
+        this._picture = '';
+        this._title = '';
+        this._category = '';
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log('add_work submit ', this._title, this._category, this._picture);
+
+        if (this._picture && this._title && this._category) {
+            this.form_data.append('title', this._title);
+            this.form_data.append('category', this._category);
+            this.form_data.append('image', this._picture);
+
+            workFormBus.emit('workForm:createWork', this.form_data);
+            console.log('form_data send:', this.form_data); // comprend pas pourquoi form_data s'affiche vide alors qu'il est remplie 
+        }
+    };
+
+    init() {
+        console.log('Initializing WorkForm...');
+        title.addEventListener('input', () => {
+            workFormBus.emit('workForm:titleAdded', title.value);
+        });
+        this._addWorkForm.addEventListener('submit', this.onSubmit.bind(this));
+    }
+}
+
+export const workForm = new WorkForm();
