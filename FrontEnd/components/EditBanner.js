6f1916c964b4cbb4a0d7d7538c3fd832d0512a50
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-03 18:04:30 +0100

    ajout 2e boutton modifier

--- FrontEnd/components/EditBanner.js
+++ FrontEnd/components/EditBanner.js
@@ -13,12 +13,17 @@ import { modalEvent } from "./ModalViewController.js";
 export class EditBanner {
     constructor() {
         this._edit_banner = document.querySelector('.edit__banner');
-        this._edit_btn = document.querySelector('.edit__btn');
-        this._edit_btn.addEventListener('click', () => modalEvent.openModal());
+        this._edit_btn = document.querySelectorAll('.edit__btn');
+        this._edit_btn.forEach(btn => {
+            btn.addEventListener('click', () => modalEvent.openModal());
+        });
         console.log('EditBanner executé');
     }
-
+    
     display() {
         this._edit_banner.style.display = 'flex';
+        this._edit_btn.forEach(btn => {
+            btn.style.display = 'flex';
+        });
     }
 };
