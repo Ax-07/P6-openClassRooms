e8dc776efbdc301ce2b4cf495f1b3c56e4b33337
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-24 18:00:09 +0100

    ajout condition upload et modif display en class css

--- FrontEnd/components/AddPicture.js
+++ FrontEnd/components/AddPicture.js
@@ -6,17 +6,14 @@ class AddPicture {
         this.add_picture_wrapper = document.querySelector('.add-picture__wrapper');
         this.add_picture_input = document.querySelector('.add-picture__input');
         this.add_picture_preview = document.querySelector('.add-picture__preview');
-
     }
 
     displayPreviewPicture() {
-        this.add_picture_preview.style.display = 'block';
-        this.add_picture_wrapper.style.display = 'none';
+        this.add_picture_preview.classList.add('add-picture__preview--active');
     }
 
     hidePreviewPicture() {
-        this.add_picture_preview.style.display = 'none';
-        this.add_picture_wrapper.style.display = 'flex';
+        this.add_picture_preview.classList.remove('add-picture__preview--active');
     }
 
     setPreviewPicture(reader) {
@@ -31,6 +28,11 @@ class AddPicture {
     handleInputChange(e) {
         this._file = e.target.files[0];
         if (this._file) {
+            const fileSizeInMB = this._file.size / (1024 * 1024); console.log('fileSizeInMB:', fileSizeInMB);
+            if (fileSizeInMB > 4) {
+                alert("Le fichier est trop grand. Veuillez choisir un fichier de moins de 4 Mo.");
+                return;
+            }
             const reader = new FileReader();
             reader.addEventListener('load', () => {
                 this.add_picture_preview.setAttribute('src', reader.result);
@@ -44,8 +46,6 @@ class AddPicture {
     init() {
         console.log('Initializing AddPicture...');
         this.add_picture_input.addEventListener('change', this.handleInputChange.bind(this));
-
-        // Additional initialization logic can be added here.
     }
 }
 
