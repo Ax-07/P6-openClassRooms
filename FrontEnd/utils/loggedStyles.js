d14bac16071423a14c51117b592ba40ed9ece4ac
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-24 18:10:35 +0100

    ajout des elements dont le style doit etre modifer si connecter

--- FrontEnd/utils/loggedStyles.js
+++ FrontEnd/utils/loggedStyles.js
@@ -1,14 +1,18 @@
-/*header*/
-/*introduction*/
+/* header */
+/* introduction */
+/* portfolio */
 /*filter*/
-/*section title*/
 
 export const setLoggedStyles = () => {
     const header = document.querySelector('.header');
     header.classList.add('header--logged');
 
-
     const introdution = document.querySelector('#introduction');
     introdution.classList.add('introduction--logged');
-    introdution.style.margin = '0 0 139px 0';
+
+    const filter = document.querySelector('.filter');
+    filter.classList.add('filter--logged');
+
+    const portfolio = document.querySelector('#portfolio');
+    portfolio.classList.add('portfolio--logged');
 }
